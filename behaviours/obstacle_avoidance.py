f330ed51fefd911016b0ea1fb2962c9cb2bdc6c1
Jeanne <jeanne.boussonniere@gmail.com>
2026-07-27 12:23:26 +0200

    Adjust backward trigger

--- behaviours/obstacle_avoidance.py
+++ behaviours/obstacle_avoidance.py
@@ -10,9 +10,9 @@ class ObstacleAvoidance:
         self.count_turn = 0
         self.max_backward = 10
         self.min_turn = 5
-        self.max_turn = 35
+        self.max_turn = 15
         self.backward = False
-        self.max_prox_center = 2000
+        self.max_prox_center = 2500
         self.max_prox_side = 800
         self.turn_direction = None
 
@@ -21,8 +21,9 @@ class ObstacleAvoidance:
         left = prox[0] + prox[1]
         right = prox[3] + prox[4]
         closer = max(prox[:5])
+        factor = self._factor(closer)
         if not self.backward: 
-            if prox[2] >= self.max_prox_center or (left >= self.max_prox_center and right >= self.max_prox_center):
+            if closer >= self.max_prox_center or (left >= self.max_prox_center and right >= self.max_prox_center):
                 self.backward = True
                 self.turn_direction = None
                 self.count_turn = 0
@@ -36,10 +37,8 @@ class ObstacleAvoidance:
                         self.turn_direction = "right"
 
                     if self.turn_direction == "right":
-                        factor = self._factor(closer)
                         return self.wheel_velocity, self.wheel_velocity * max(0, 1 - 2 * factor)
                     else:
-                        factor = self._factor(closer)
                         return self.wheel_velocity * max(0, 1 - 2 * factor), self.wheel_velocity
                 elif self.turn_direction is not None and self.count_turn >= self.max_turn:
                     self.backward = True
@@ -49,20 +48,16 @@ class ObstacleAvoidance:
                 else:
                     self.count_turn = 0
                     if left > right:
-                        factor = self._factor(closer)
                         self.turn_direction = "right"
                         return self.wheel_velocity, self.wheel_velocity * max(0, 1 - 2 * factor)
                     else:
-                        factor = self._factor(closer)
                         self.turn_direction = "left"
                         return self.wheel_velocity * max(0, 1 - 2 * factor), self.wheel_velocity
             elif self.turn_direction is not None and self.count_turn < self.min_turn:
                 self.count_turn += 1
                 if self.turn_direction == "right":
-                    factor = self._factor(closer)
                     return self.wheel_velocity, self.wheel_velocity * max(0, 1 - 2 * factor)
                 else:
-                    factor = self._factor(closer)
                     return self.wheel_velocity * max(0, 1 - 2 * factor), self.wheel_velocity
             else:  
                 self.turn_direction = None
